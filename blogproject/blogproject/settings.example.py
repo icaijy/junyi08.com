aa44d6472cee7bcf13e1be23792631fd88996c15
icaijy <caijunyi08@outlook.com>
2020-11-17 17:05:15 +0800

    change to font-awesome 5

--- blogproject/blogproject/settings.example.py
+++ blogproject/blogproject/settings.example.py
@@ -26,7 +26,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'junyi08.com']
+ALLOWED_HOSTS = ['localhost', 'junyi08.com','127.0.0.1']
 
 
 # Application definition
